200a85b1ca8bf390c9ec87ad29efae36664faeed
Vahik Thmassians <vahikt@gmail.com>
2020-12-18 17:54:42 -0800

    test configuration add

---
+++ import/rightsline/report/class_configuration.php
@@ -0,0 +1,144 @@
+<?php
+Array
+(
+    [parent_id] => 0
+    [id] => 1
+    [item_type_id] => 
+    [locale_id] => 
+    [label] => Rightsline Report
+    [namespace] => namespace
+    [identifier] => ::DRIVER::IMPORT::
+    [external_id] => 
+    [md5] => 
+    [sort_order] => 
+    [date_created] => 2019-03-04 04:19:06
+    [date_updated] => 2020-12-15 16:36:48
+    [driver_feed_class] => 
+    [driver_ingest_class] => \Vampire\Driver\Rightsline\Ingest\Report
+    [driver_gallery_class] => \Vampire\Driver\Core\Artwork\PassThru
+    [driver_collection_class] => 
+    [driver_import_class] => \Vampire\Driver\Core\Import\Standard
+    [driver_transform_class] => \Vampire\Driver\Rightsline\Transform\Report
+    [driver_type] => ::DRIVER::IMPORT::
+    [driver_name] => Rightsline Report
+    [driver_template_type] => multi_file
+    [omit_parents] => y
+    [driver_constant] => ::DRIVER::RIGHTSLINE_REPORT::
+    [driver_configuration] => testing 123 asdfasdf
+    [driver_field_map] => {
+	"episode": {
+		"season_number": "season_number",
+		"episode_number": "episode_number",
+		"production_year": "production_year",
+		"original_language": "original_language",
+		"episode_name": "episode_name",
+		"original_air_date": "original_air_date",
+		"original_country": "country_of_origin",
+		"rating_ustv": "us_tv_rating",
+		"title": "title",
+		"display_title": "title",
+		"tms_id": "tms_id",
+		"cms_id": "cms_id",
+		"amazon_id": "amazon_id",
+		"rightsline_id": "catalog_id",
+		"inventory_id": "inventory_id"
+	},
+	"season": {
+		"title": "title",
+		"rightsline_id": "catalog_id",
+		"synopsis_web": "web_synopsis",
+		"season_number": "season_number",
+		"original_air_date": "original_air_date",
+		"original_country": "country_of_origin",
+		"original_language": "original_language",
+		"rating_ustv": "us_tv_rating",
+		"rating_mpaa": "mpaa_rating",
+		"display_title": "display_title",
+		"production_year": "production_year",
+		"tms_id": "tms_id",
+		"cms_id": "cms_id",
+		"amazon_id": "amazon_id",
+		"rightsline_template_name": "catalog_template",
+		"rightsline_inventory_id": "inventory_id"
+	},
+	"series": {
+		"title": "title",
+		"rating_mpaa": "mpaa_rating",
+		"promotions": "promotions",
+		"tagline": "tagline",
+		"original_country": "country_of_origin",
+		"original_language": "original_language",
+		"original_air_date": "original_air_date",
+		"display_title": "display_title",
+		"rating_ustv": "us_tv_rating",
+		"title_brief_9": "title__brief_9_max",
+		"title_brief_16": "title__brief_16_max",
+		"title_brief_20": "title__brief_20_character_max",
+		"tms_id": "tms_id",
+		"cms_id": "cms_id",
+		"amazon_id": "amazon_id",
+		"parent_id": "parent_catalog_id",
+		"rightsline_inventory_id": "inventory_id",
+		"rightsline_template_name": "catalog_template",
+		"rightsline_id": "catalog_id"
+	},
+	"feature": {
+		"production_year": "production_year",
+        "production_country" : "blank",
+		"original_country": "country_of_origin",
+		"original_air_date": "original_air_date",
+		"rating_ustv": "us_tv_rating",
+		"rating_mpaa": "mpaa_rating",
+		"original_language": "original_language",
+		"display_title": "display_title",
+		"rightsline_id": "catalog_id",
+		"rightsline_template_name": "catalog_template",
+		"cms_id": "cms_id",
+		"tms_id": "tms_id",
+		"amazon_id": "amazon_id",
+		"title": "title",
+		"title_brief_16": "title__brief_16_max",
+		"title_brief_20": "title__brief_20_character_max",
+		"rightsline_inventory_id": "inventory_id"
+	},
+	"inventory": {
+		"type": "type",
+		"offline_viewing": "temporary_download__offline_viewing",
+		"tms_id": "tms_id",
+		"amazon_id": "amazon_id",
+		"runtime_seconds": "runtime__sec_",
+		"runtime_minutes": "runtime__min_",
+		"release_date": "release_date",
+		"sku": "sku",
+		"additional_sku": "additional_sku",
+		"psvod_sku": "psvod_sku",
+		"cms_id": "cms_id",
+		"apple_id": "apple_id",
+		"resolution": "resolution",
+		"apect_ratio": "aspect_ratio",
+		"id16x9": "id16x9",
+		"closed_caption": "closed_caption",
+		"audio_format": "audio_format",
+		"caption_language": "caption_language",
+        "inventory_id" : "inventory_id"
+	},
+	"talent": {
+		"actor": "actor_list",
+		"writer": "writer_list",
+		"director": "director_list",
+		"creator": "creator_list"
+	},
+	"translation": {
+		"synopsis_short": "Synopsis - Short (Feature Max:149, Series Max: 170)",
+		"synopsis_medium": "Synopsis - Medium (Feature Max: 240, Series Max: 400)",
+		"synopsis": "Synopsis - Long"
+	}
+}
+    [driver_gallery_id] => 4003
+    [mode] => 100
+    [limit] => 20
+    [channel_id] => 
+    [entity_type_id] => 11
+)
+
+?>
